975ec52d34fc30a752a3de9441ae56584f867f70
Shady Ragab <eng.shady.ragab.1@gmail.com>
2020-05-29 14:57:18 +0200

    add Block hound and pom file

---
+++ src/test/java/com/shady/BlockHound.java
@@ -0,0 +1,51 @@
+package com.shady;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class BlockHound {
+
+
+    private static void accept(String x) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    public void testAllowed(){
+
+        Mono<String> mono = Mono.just("a").doOnNext(BlockHound::accept).subscribeOn(Schedulers.newElastic("elastic"));
+
+        StepVerifier.create(mono).expectNext("a").verifyComplete();
+    }
+
+    @Test
+    public void testNotAllowed(){
+
+       // reactor.blockhound.BlockHound.install();
+
+      /*  Mono.delay(Duration.ofSeconds(1))
+                .doOnNext(it -> {
+                    try {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .block();*/
+    }
+
+}
